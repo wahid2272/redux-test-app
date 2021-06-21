05575294219fb3fd4dc8799de19efff6f5455eca
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-06-21 14:45:28 +0300

    redux saga is used to fetch api and user component state management

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -6,24 +6,24 @@ import Card from './CardComponent';
 const Users = () => {
   const dispatch = useDispatch();
   const users = useSelector(state => state.users.users);
+  const loading = useSelector(state => state.users.loading);
+  const error = useSelector(state => state.users.error);
 
   useEffect(() => {
-    dispatch(getUsers([{
-      id: 1,
-      name: 'Nabil Ahmed',
-      company: {
-        name: 'AY Office oy',
-        catchPhrase: 'Multi-layered Client-server neural-net',
-      }
-    }]))
+    dispatch(getUsers());
   }, [dispatch]);
 
   return (
     <div>
+      {users.loading && <p>Loading...</p>}
+
+      {users.length === 0 && !loading && <p>No users available!</p>}
+
+      {error && !loading && <p>{error}</p>}
+      
       {users.length > 0 && users.map((user) => (
         <Card key={user.id} user={user} />
       ))}
-      {users.length === 0 && <p>No users available!</p>}
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index.js';
+import rootSaga from './sagas/index';
 
-//const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer, ['redux']
-+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   rootReducer, ['redux']
+// +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers( applyMiddleware(sagaMiddleware)
+));
+
+sagaMiddleware.run(rootSaga);
 
 export default store; 
